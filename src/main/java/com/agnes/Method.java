51b65965f6cd47fdb70c30c37a3c377144147f74
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-21 20:15:25 +0100

    created a class for methods

---
+++ src/main/java/com/agnes/Method.java
@@ -0,0 +1,14 @@
+package com.agnes;
+
+public class Method {
+    public static void main (String[]args) {
+        exe1();
+    }
+   public static void exe1() {
+        /*Write a method that takes two double parameters and returns their product.
+         * Call this method in the main program, passing two values, and print the result.
+         *Write a method that checks whether a number is even or odd.
+         *The method should return a boolean (true if even, false if odd).
+         */
+   }
+}
